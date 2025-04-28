4616ca78205f429b939d6c0aa10a5d32711ca287
BlueCannonBall <mmttiizz100@gmail.com>
2025-04-28 11:52:37 -0400

    Fix keyboard example

--- examples/keyboard.rs
+++ examples/keyboard.rs
@@ -1,10 +1,18 @@
-use input_device::{InputSimulator, SimulationError};
+use input_device::{InputSimulator, SimulationError, Key};
 
 pub fn main() -> Result<(), SimulationError> {
     let mut sim = InputSimulator::new()?;
     std::thread::sleep(std::time::Duration::from_secs(1));
     sim.key_down(Key::A)?;
-    std::thread::sleep(std::time::Duration::from_secs(3));
+    std::thread::sleep(std::time::Duration::from_millis(50));
     sim.key_up(Key::A)?;
+    std::thread::sleep(std::time::Duration::from_millis(50));
+    sim.key_down(Key::B)?;
+    std::thread::sleep(std::time::Duration::from_millis(50));
+    sim.key_up(Key::B)?;
+    std::thread::sleep(std::time::Duration::from_millis(50));
+    sim.key_down(Key::C)?;
+    std::thread::sleep(std::time::Duration::from_millis(50));
+    sim.key_up(Key::C)?;
     Ok(())
 }
