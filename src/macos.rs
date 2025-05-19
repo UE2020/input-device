08769d857d7246c657802f8eb7e327de0ddbbcd2
Aspect <ttmmiizz101@gmail.com>
2025-05-18 21:01:32 -0400

    Add rudimentary macOS support

--- src/macos.rs
+++ src/macos.rs
@@ -1,1 +1,32 @@
+use core_graphics::event::*;
+use core_graphics::event_source::*;
+use core_graphics::geometry::CGPoint;
 
+use crate::Key;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum SimulationError {
+    #[error("Windows error: {0}")]
+    WindowsError(#[from] windows::core::Error),
+}
+
+pub(crate) struct PlatformImpl;
+
+impl PlatformImpl {
+    pub(crate) fn new() -> Result<Self, SimulationError> {
+        Self
+    }
+
+    pub(crate) fn move_mouse_abs() -> Result<(), SimulationError> {
+        let source = CGEventSource::new(CGEventSourceStateID::HIDSystemState).unwrap();
+        let event = CGEvent::new_mouse_event(
+            &source,
+            CGEventType::MouseMoved,
+            CGPoint::new(x, y),
+            CGMouseButton::Left,
+        ).unwrap();
+
+        event.post(CGEventTapLocation::HID);
+    }
+}
