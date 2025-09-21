576405a701e6e6c45d9af3e5c89a9a1442c05d21
Aspect <ttmmiizz101@gmail.com>
2025-09-20 21:47:20 -0400

    Update touch interval

--- src/windows.rs
+++ src/windows.rs
@@ -150,7 +150,7 @@ impl PlatformImpl {
             }
 
             drop(guard);
-            std::thread::sleep(std::time::Duration::from_millis(1));
+            std::thread::sleep(std::time::Duration::from_millis(8));
         });
 
         Ok(Self {
